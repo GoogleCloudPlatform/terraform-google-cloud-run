1cd4c640e0e4814bedcc920bd3129474c05d8047
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2022-06-09 14:33:19 -0300

    Adds meta info

--- modules/secure-cloud-run-security/versions.tf
+++ modules/secure-cloud-run-security/versions.tf
@@ -27,4 +27,12 @@ terraform {
       version = ">= 3.53, < 5.0"
     }
   }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-cloud-run:secure-cloud-run-security/v0.3.0"
+  }
+
+  provider_meta "google-beta" {
+    module_name = "blueprints/terraform/terraform-google-cloud-run:secure-cloud-run-security/v0.3.0"
+  }
 }
