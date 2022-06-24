7980f07acb4f3c52f050f20a593a631f686a8047
Samir-Cit <samir@ciandt.com>
2022-06-24 13:45:18 -0300

    Using module to create firewall rules.

--- modules/secure-cloud-run-net/firewall.tf
+++ modules/secure-cloud-run-net/firewall.tf
@@ -18,124 +18,100 @@ locals {
   tags = ["vpc-connector"]
 }
 
-resource "google_compute_firewall" "serverless_to_vpc_connector" {
+module "firewall_rules" {
   count = var.connector_on_host_project ? 0 : 1
 
-  project       = var.vpc_project_id
-  name          = "serverless-to-vpc-connector"
-  network       = var.shared_vpc_name
-  direction     = "INGRESS"
-  source_ranges = ["107.178.230.64/26", "35.199.224.0/19"]
-  target_tags   = local.tags
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
+  source       = "terraform-google-modules/network/google//modules/firewall-rules"
+  project_id   = var.vpc_project_id
+  network_name = var.shared_vpc_name
 
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["667"]
-  }
-
-  allow {
-    protocol = "udp"
-    ports    = ["665", "666"]
-  }
-}
-
-resource "google_compute_firewall" "vpc_connector_to_serverless" {
-  count = var.connector_on_host_project ? 0 : 1
-
-  project       = var.vpc_project_id
-  name          = "vpc-connector-to-serverless"
-  network       = var.shared_vpc_name
-  direction     = "EGRESS"
-  source_ranges = ["107.178.230.64/26", "35.199.224.0/19"]
-  target_tags   = local.tags
-
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["667"]
-  }
-
-  allow {
-    protocol = "udp"
-    ports    = ["665", "666"]
-  }
-}
-
-resource "google_compute_firewall" "vpc_connector_to_lb" {
-  count = var.connector_on_host_project ? 0 : 1
-
-  project     = var.vpc_project_id
-  name        = "vpc-connector-to-lb"
-  network     = var.shared_vpc_name
-  direction   = "EGRESS"
-  target_tags = local.tags
-
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-}
-
-resource "google_compute_firewall" "vpc_connector_health_checks" {
-  count = var.connector_on_host_project ? 0 : 1
-
-  project       = var.vpc_project_id
-  name          = "vpc-connector-health-checks"
-  network       = var.shared_vpc_name
-  direction     = "INGRESS"
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "108.170.220.0/23"]
-  target_tags   = local.tags
-
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["667"]
-  }
-}
-
-resource "google_compute_firewall" "vpc_connector_requests" {
-  count = var.connector_on_host_project ? 0 : 1
-
-  project     = var.vpc_project_id
-  name        = "vpc-connector-requests"
-  network     = var.shared_vpc_name
-  direction   = "INGRESS"
-  source_tags = local.tags
-
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-  }
-
-  allow {
-    protocol = "udp"
-  }
+  rules = [{
+    name                    = "serverless-to-vpc-connector"
+    direction               = "INGRESS"
+    ranges                  = ["107.178.230.64/26", "35.199.224.0/19"]
+    target_tags             = local.tags
+    allow = [{
+      protocol = "icmp"
+    },
+    {
+      protocol = "tcp"
+      ports    = ["667"]
+    },
+    {
+      protocol = "udp"
+      ports    = ["665", "666"]
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  },
+  {
+    name                    = "vpc-connector-to-serverless"
+    direction               = "EGRESS"
+    ranges                  = ["107.178.230.64/26", "35.199.224.0/19"]
+    target_tags             = local.tags
+    allow = [{
+      protocol = "icmp"
+    },
+    {
+      protocol = "tcp"
+      ports    = ["667"]
+    },
+    {
+      protocol = "udp"
+      ports    = ["665", "666"]
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  },
+  {
+    name                    = "vpc-connector-to-lb"
+    direction               = "EGRESS"
+    ranges                  = []
+    target_tags             = local.tags
+    allow = [{
+      protocol = "tcp"
+      ports    = ["80"]
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  },
+  {
+    name                    = "vpc-connector-health-checks"
+    direction               = "INGRESS"
+    ranges                  = ["130.211.0.0/22", "35.191.0.0/16", "108.170.220.0/23"]
+    target_tags             = local.tags
+    allow = [{
+      protocol = "tcp"
+      ports    = ["667"]
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  },
+  {
+    name                    = "vpc-connector-requests"
+    direction               = "INGRESS"
+    ranges                  = []
+    source_tags             = local.tags
+    allow = [{
+      protocol = "icmp"
+    },
+    {
+      protocol = "tcp"
+    },
+    {
+      protocol = "udp"
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  }]
 }
