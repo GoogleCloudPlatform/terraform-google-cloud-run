d4ed7cef12160aeef2e569f86eb6c7cdcb29f3f7
Renato Rudnicki <renatojr@ciandt.com>
2022-08-31 14:59:51 -0300

    adding missing variables on main.tf

--- modules/secure-cloud-run-core/main.tf
+++ modules/secure-cloud-run-core/main.tf
@@ -17,14 +17,33 @@
 module "cloud_run" {
   source = "../.."
 
-  service_name          = var.service_name
-  project_id            = var.project_id
-  location              = var.location
-  image                 = var.image
-  service_account_email = var.cloud_run_sa
-  encryption_key        = var.encryption_key
-  members               = var.members
-  env_vars              = var.env_vars
+  service_name           = var.service_name
+  project_id             = var.project_id
+  location               = var.location
+  image                  = var.image
+  service_account_email  = var.cloud_run_sa
+  encryption_key         = var.encryption_key
+  members                = var.members
+  env_vars               = var.env_vars
+  generate_revision_name = var.generate_revision_name
+  traffic_split          = var.traffic_split
+  service_labels         = var.service_labels
+  template_labels        = var.template_labels
+  container_concurrency  = var.container_concurrency
+  timeout_seconds        = var.timeout_seconds
+  volumes                = var.volumes
+  limits                 = var.limits
+  requests               = var.requests
+  ports                  = var.ports
+  argument               = var.argument
+  container_command      = var.container_command
+  env_secret_vars        = var.env_secret_vars
+  volume_mounts          = var.volume_mounts
+  verified_domain_name   = var.verified_domain_name
+  force_override         = var.force_override
+  certificate_mode       = var.certificate_mode
+  domain_map_labels      = var.domain_map_labels
+  domain_map_annotations = var.domain_map_annotations
 
   service_annotations = {
     "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
