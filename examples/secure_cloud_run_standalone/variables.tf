f50f56d9716517e2784f08047f94c2b5c4daba75
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2022-11-01 17:28:44 -0300

    Undo variable type change

--- examples/secure_cloud_run_standalone/variables.tf
+++ examples/secure_cloud_run_standalone/variables.tf
@@ -54,7 +54,7 @@ variable "access_level_members" {
 
 variable "domain" {
   description = "Domain name to run on the load balancer on."
-  type        = list(string)
+  type        = string
 }
 
 variable "egress_policies" {
