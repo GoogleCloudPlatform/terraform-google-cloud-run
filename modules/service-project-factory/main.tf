3faedb5c67ebbf17ff48fe92b3bf9a13ba7893d3
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2023-06-19 16:31:23 -0300

    fix: adds variable to not disable APIs on destroy (#126)

--- modules/service-project-factory/main.tf
+++ modules/service-project-factory/main.tf
@@ -19,14 +19,15 @@ locals {
   api = var.serverless_type == "CLOUD_RUN" ? "run" : "cloudfunctions"
 }
 module "serverless_project" {
-  source            = "terraform-google-modules/project-factory/google"
-  version           = "~> 14.2"
-  random_project_id = "true"
-  activate_apis     = var.activate_apis
-  name              = var.project_name
-  org_id            = var.org_id
-  billing_account   = var.billing_account
-  folder_id         = var.folder_name
+  source                      = "terraform-google-modules/project-factory/google"
+  version                     = "~> 14.2"
+  random_project_id           = "true"
+  activate_apis               = var.activate_apis
+  name                        = var.project_name
+  org_id                      = var.org_id
+  billing_account             = var.billing_account
+  folder_id                   = var.folder_name
+  disable_services_on_destroy = var.disable_services_on_destroy
 
   svpc_host_project_id = var.network_project_id
   grant_network_role   = var.network_project_id != "" ? true : false
