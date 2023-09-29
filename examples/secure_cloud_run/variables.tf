a18845d59bf74442363e05d41f41a2ed254cb835
Andrew Peabody <andrewpeabody@google.com>
2023-09-29 15:30:16 +0000

    fix: remove duplicate variable key

--- examples/secure_cloud_run/variables.tf
+++ examples/secure_cloud_run/variables.tf
@@ -98,7 +98,6 @@ variable "groups" {
     group_serverless_administrator          = optional(string, null)
     group_serverless_security_administrator = optional(string, null)
     group_cloud_run_developer               = optional(string, null)
-    group_cloud_run_developer               = optional(string, null)
     group_cloud_run_user                    = optional(string, null)
   })
 
