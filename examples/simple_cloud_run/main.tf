284e112112e7b4c6c50c75bba06434dcdc3ef28f
Andrew Peabody <andrewpeabody@google.com>
2023-11-01 10:21:02 -0700

    Update main.tf

--- examples/simple_cloud_run/main.tf
+++ examples/simple_cloud_run/main.tf
@@ -16,7 +16,7 @@
 
 module "service_account" {
   source     = "terraform-google-modules/service-accounts/google"
-  version    = "~> 4.2.0"
+  version    = "~> 4.2"
   project_id = var.project_id
   prefix     = "sa-cloud-run"
   names      = ["simple"]
