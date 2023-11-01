1e46d3d963895d2b531ebc5d19f1375b72b425d9
Andrew Peabody <andrewpeabody@google.com>
2023-11-01 10:21:10 -0700

    Update main.tf

--- examples/simple_cloud_run_with_cmek/main.tf
+++ examples/simple_cloud_run_with_cmek/main.tf
@@ -20,7 +20,7 @@ locals {
 
 module "service_account" {
   source     = "terraform-google-modules/service-accounts/google"
-  version    = "~> 4.2.0"
+  version    = "~> 4.2"
   project_id = var.project_id
   prefix     = "sa-cloud-run"
   names      = ["cmek"]
