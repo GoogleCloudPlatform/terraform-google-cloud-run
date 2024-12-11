f1dda93c0b493671c816eb845257270c684e418f
Andrew Peabody <andrewpeabody@google.com>
2024-12-11 19:37:24 +0000

    fix: remove create_ssl_certificate (used with private_key/certificate)

--- modules/secure-cloud-run-core/loadbalancer.tf
+++ modules/secure-cloud-run-core/loadbalancer.tf
@@ -27,7 +27,6 @@ module "lb-http" {
   ssl                             = true
   managed_ssl_certificate_domains = var.ssl_certificates.generate_certificates_for_domains
   ssl_certificates                = var.ssl_certificates.ssl_certificates_self_links
-  create_ssl_certificate          = length(var.ssl_certificates.generate_certificates_for_domains) == 0 ? true : false
   https_redirect                  = false
   http_forward                    = false
 
