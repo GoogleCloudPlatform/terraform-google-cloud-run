2f476a08483dcb88257a85b0b963176eabf35f17
Deepraj <deeprajp@google.com>
2025-06-11 11:42:45 +0000

    IAP integration in Cloud Run ADC modules

--- modules/v2/main.tf
+++ modules/v2/main.tf
@@ -398,6 +398,6 @@ resource "google_cloud_run_v2_service_iam_member" "authorize_iap_p4sa" {
   project  	= google_cloud_run_v2_service.main.project
   name     	= google_cloud_run_v2_service.main.name
   role     	= "roles/run.invoker"
-  member   	= google_project_service_identity.iap_p4sa.member
+  member        = google_project_service_identity.iap_p4sa[count.index].member
   depends_on    = [time_sleep.wait_5m]
 }
