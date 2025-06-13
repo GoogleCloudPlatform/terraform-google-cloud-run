1d551130d3264ed07f62238c3e550f5c33530299
Deepraj <deeprajp@google.com>
2025-06-13 06:32:08 +0000

    lint error fix

--- modules/v2/variables.tf
+++ modules/v2/variables.tf
@@ -337,7 +337,7 @@ variable "execution_environment" {
 }
 
 variable "iap_members" {
-  type	     = list(string)
+  type       = list(string)
   descrption = "Users/SAs to be given httpsResourceAccessor access to the service. IAP will be enabled when Users/SAs are entered"
   default    = []
 }
