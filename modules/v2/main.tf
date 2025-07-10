760d52cd3f7f22fdf68e9fada787d9ade8723f8c
abhishek kumar tiwari <abhishektiwari571@gmail.com>
2025-07-10 11:20:03 +0530

    Update modules/v2/main.tf

--- modules/v2/main.tf
+++ modules/v2/main.tf
@@ -170,7 +170,7 @@ resource "google_cloud_run_v2_service" "main" {
           # Setting limits to null when no values are provided prevents a permanent diff
           # where the provider attempts to remove default values set by the API.
           limits = try(containers.value.resources.limits, null) != null ? {
-            cpu              = containers.value.resources.limits.cpu,
+            cpu              = try(containers.value.resources.limits.cpu, null),
             memory           = containers.value.resources.limits.memory,
             "nvidia.com/gpu" = try(containers.value.resources.limits.nvidia_gpu, null)
           } : null
