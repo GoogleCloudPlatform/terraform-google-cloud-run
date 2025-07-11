5f44465a33011c64f11d9b1c29d4d4f38674590e
Zheng Qin <zhengqin@google.com>
2025-07-11 14:20:53 -0400

    update locals refernces

--- test/setup/iam.tf
+++ test/setup/iam.tf
@@ -80,7 +80,7 @@ locals {
 
   int_required_roles = concat([
     "roles/cloudkms.admin",
-  ], flatten(values(locals.per_module_roles)))
+  ], flatten(values(local.per_module_roles)))
 }
 
 resource "google_service_account" "int_test" {
