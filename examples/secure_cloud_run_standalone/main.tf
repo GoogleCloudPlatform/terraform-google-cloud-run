9de1f923ed2ccf1cd38fe0d1fe9aa4d624c39db2
Marcos Vinicius Leal da Silva <marcosvl@ciandt.com>
2026-04-15 10:41:06 -0300

    fix: secure_cloud_run_standalone example to use new modules

--- examples/secure_cloud_run_standalone/main.tf
+++ examples/secure_cloud_run_standalone/main.tf
@@ -27,8 +27,7 @@ resource "random_id" "random_folder_suffix" {
 }
 
 module "secure_harness" {
-  source  = "GoogleCloudPlatform/cloud-run/google//modules/secure-serverless-harness"
-  version = "~> 0.16"
+  source = "marcos-leal-cit/cloud-run/google//modules/secure-serverless-harness"
 
   billing_account                             = var.billing_account
   security_project_name                       = "prj-kms-secure-cloud-run"
@@ -66,8 +65,7 @@ resource "null_resource" "copy_image" {
 }
 
 module "secure_cloud_run" {
-  source  = "GoogleCloudPlatform/cloud-run/google//modules/secure-cloud-run"
-  version = "~> 0.16"
+  source = "marcos-leal-cit/cloud-run/google//modules/secure-cloud-run"
 
   location                                = local.location
   region                                  = local.region
