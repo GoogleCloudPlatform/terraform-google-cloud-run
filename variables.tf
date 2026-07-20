ab5f835205b7ffc5527c9da42f458e44e48620ef
acustodio <acustodio@ciandt.com>
2026-07-20 11:09:13 -0300

    linter: lint fixes

--- variables.tf
+++ variables.tf
@@ -286,8 +286,3 @@ variable "members" {
   default     = []
 }
 
-variable "ssl" {
-  description = "Enables the Post-Quantum Cryptography (PQC) SSL Policy for the Load Balancer (Requires TLS 1.3)."
-  type        = bool
-  default     = false
-}
